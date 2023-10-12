629f227e0f85100464c35495a429207ed9e07f99
Arttu Valo <arttu.valo@gmail.com>
2023-10-12 03:34:34 +0300

    Improved XML resource loading.

--- app/src/main/java/multiproject/app/App.java
+++ app/src/main/java/multiproject/app/App.java
@@ -8,11 +8,47 @@ import static multiproject.app.MessageUtils.getMessage;
 
 import org.apache.commons.text.WordUtils;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class App {
   public static void main(String[] args) {
+    printLocale("Default locale", Locale.getDefault());
+    printLocale("Root locale", Locale.ROOT);
+    printLocale("English locale", Locale.ENGLISH);
+    printLocale("French locale", Locale.FRENCH);
+    printLocale("US locale", Locale.US);
+    printLocale("UK locale", Locale.UK);
+    printLocale("France locale", Locale.FRANCE);
+    List<Locale> sortedLocales = Arrays.stream(Locale.getAvailableLocales())
+      .sorted(Comparator.comparing(Locale::toString))
+      .collect(Collectors.toList());
+    System.out.println("Available locales:");
+    for (Locale locale : sortedLocales) {
+      printLocale("Locale", locale);
+    }
     LinkedList tokens;
     tokens = split(getMessage());
     String result = join(tokens);
     System.out.println(WordUtils.capitalize(result));
+  }
+
+  private static void printLocale(String label, Locale locale) {
+    System.out.printf("%s: %s%n\tName: %s%n", label, locale, locale.getDisplayName());
+    if (!locale.getLanguage().isBlank()) {
+      System.out.printf("\tLanguage: %s (%s)%n", locale.getDisplayLanguage(), locale.getLanguage());
+    }
+    if (!locale.getCountry().isBlank()) {
+      System.out.printf("\tCountry: %s (%s)%n", locale.getDisplayCountry(), locale.getCountry());
+    }
+    if (!locale.getVariant().isBlank()) {
+      System.out.printf("\tVariant: %s (%s)%n", locale.getDisplayVariant(), locale.getVariant());
+    }
+    if (!locale.getScript().isBlank()) {
+      System.out.printf("\tScript: %s (%s)%n", locale.getDisplayScript(), locale.getScript());
+    }
   }
 }
--- resourcebundle/src/test/java/test/XMLResourceBundleUnitTest.java
+++ resourcebundle/src/test/java/test/XMLResourceBundleUnitTest.java
@@ -15,18 +15,33 @@ public class XMLResourceBundleUnitTest {
     try(InputStream inputstream = this.getClass().getResourceAsStream("/labels_fr.xml")) {
       Properties props = new Properties();
       props.loadFromXML(inputstream);
-      assertEquals("la valeur de la cle 1", props.getProperty("maCle1"));
-      assertEquals("la valeur de la cle 2", props.getProperty("maCle2"));
-      assertEquals("la valeur de la cle 3", props.getProperty("maCle3"));
+      assertEquals("La valeur de la clé 1", props.getProperty("myKey1"));
+      assertEquals("La valeur de la clé 2", props.getProperty("myKey2"));
+      assertEquals("La valeur de la clé 3", props.getProperty("myKey3"));
     }
   }
 
   @Test
   public void loadPropertiesAsResourceBundle() {
-    Locale frLocale = new Locale("fr");
-    ResourceBundle bundle = ResourceBundle.getBundle("labels", frLocale, new XMLResourceBundleControl());
-    assertEquals("la valeur de la cle 1", bundle.getString("maCle1"));
-    assertEquals("la valeur de la cle 2", bundle.getString("maCle2"));
-    assertEquals("la valeur de la cle 3", bundle.getString("maCle3"));
+    ResourceBundle bundle = ResourceBundle.getBundle("labels", Locale.ROOT, new XMLResourceBundleControl());
+    assertEquals("value of key 1", bundle.getString("myKey1"));
+    assertEquals("value of key 2", bundle.getString("myKey2"));
+    assertEquals("value of key 3", bundle.getString("myKey3"));
+  }
+
+  @Test
+  public void loadPropertiesAsResourceBundle_en() {
+    ResourceBundle bundle = ResourceBundle.getBundle("labels", Locale.ENGLISH, new XMLResourceBundleControl());
+    assertEquals("The value of key 1", bundle.getString("myKey1"));
+    assertEquals("The value of key 2", bundle.getString("myKey2"));
+    assertEquals("The value of key 3", bundle.getString("myKey3"));
+  }
+
+  @Test
+  public void loadPropertiesAsResourceBundle_fr() {
+    ResourceBundle bundle = ResourceBundle.getBundle("labels", Locale.FRENCH, new XMLResourceBundleControl());
+    assertEquals("La valeur de la clé 1", bundle.getString("myKey1"));
+    assertEquals("La valeur de la clé 2", bundle.getString("myKey2"));
+    assertEquals("La valeur de la clé 3", bundle.getString("myKey3"));
   }
 }
